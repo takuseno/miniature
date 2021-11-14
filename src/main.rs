281bb940aa232a6637fe5075163691974dd57371
takuseno <takuma.seno@gmail.com>
2021-11-14 09:35:12 +0900

    Add test training

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,34 @@ mod optimizer;
 mod parametric_functions;
 mod variable;
 
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use optimizer::OptimizerImpl;
+
 fn main() {
-    println!("Hello, world!");
+    let fc1 = parametric_functions::linear::Linear::new(16, 32);
+    let fc2 = parametric_functions::linear::Linear::new(32, 2);
+
+    let mut optim = optimizer::SGD::new(0.001);
+    optim.set_params(fc1.get_params());
+    optim.set_params(fc2.get_params());
+
+    for i in 0..1000 {
+        let x = Rc::new(RefCell::new(variable::Variable::rand(vec![32, 16])));
+        let t = Rc::new(RefCell::new(variable::Variable::new(vec![32, 2])));
+        t.borrow_mut().zeros();
+
+        // forward
+        let h = fc1.call(x);
+        let output = fc2.call(h);
+
+        // loss
+        let loss = functions::mean(functions::square(functions::sub(output, t)));
+        println!("{}", loss.borrow().data[0]);
+
+        optim.zero_grad();
+        graph::backward(loss);
+        optim.update();
+    }
 }
