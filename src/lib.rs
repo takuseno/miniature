0b339f7e47c89603c719d4543910ea4a415efb08
takuseno <takuma.seno@gmail.com>
2021-11-15 08:15:58 +0900

    Add library module

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod datasets;
+pub mod function;
+pub mod functions;
+pub mod graph;
+pub mod optimizer;
+pub mod parametric_functions;
+pub mod variable;
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-mod datasets;
-mod function;
-mod functions;
-mod graph;
-mod optimizer;
-mod parametric_functions;
-mod variable;
-
-use optimizer::OptimizerImpl;
+use miniature::datasets::MNISTLoader;
+use miniature::functions as F;
+use miniature::graph::backward;
+use miniature::optimizer as S;
+use miniature::optimizer::OptimizerImpl;
+use miniature::parametric_functions as PF;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let dataset = datasets::MNISTLoader::new("datasets")?;
+    let dataset = MNISTLoader::new("datasets")?;
     let (test_x, test_t) = dataset.get_test_data();
 
-    let fc1 = parametric_functions::linear::Linear::new(28 * 28, 128);
-    let fc2 = parametric_functions::linear::Linear::new(128, 128);
-    let fc3 = parametric_functions::linear::Linear::new(128, 10);
+    let fc1 = PF::linear::Linear::new(28 * 28, 128);
+    let fc2 = PF::linear::Linear::new(128, 128);
+    let fc3 = PF::linear::Linear::new(128, 10);
 
-    let mut optim = optimizer::SGD::new(0.001);
+    let mut optim = S::SGD::new(0.001);
     optim.set_params(fc1.get_params());
     optim.set_params(fc2.get_params());
     optim.set_params(fc3.get_params());
@@ -24,30 +21,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut iter = 0;
     loop {
         let (x, t) = dataset.sample(128);
-        let onehot_t = functions::onehot(t, 10);
+        let onehot_t = F::onehot(t, 10);
 
         // forward
-        let h1 = functions::relu(fc1.call(x));
-        let h2 = functions::relu(fc2.call(h1));
+        let h1 = F::relu(fc1.call(x));
+        let h2 = F::relu(fc2.call(h1));
         let output = fc3.call(h2);
 
         // loss
-        let cross_entropy = functions::neg(functions::mul(
-            onehot_t,
-            functions::log(functions::softmax(output)),
-        ));
-        let loss = functions::mean(cross_entropy);
+        let cross_entropy = F::neg(F::mul(onehot_t, F::log(F::softmax(output))));
+        let loss = F::mean(cross_entropy);
 
         optim.zero_grad();
-        graph::backward(loss);
+        backward(loss);
         optim.update();
 
         iter += 1;
         if iter % 100 == 0 {
             // test
-            let h1 = functions::relu(fc1.call(test_x.clone()));
-            let h2 = functions::relu(fc2.call(h1));
-            let output = functions::argmax(fc3.call(h2));
+            let h1 = F::relu(fc1.call(test_x.clone()));
+            let h2 = F::relu(fc2.call(h1));
+            let output = F::argmax(fc3.call(h2));
 
             let mut count = 0;
             let test_size = output.borrow().shape[0];
